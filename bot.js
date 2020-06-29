928d4fbedcd41af02a3ce9c642ec5331d9f0d4d9
daxherrera <daxherrera@gmail.com>
2020-06-29 01:27:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,26 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+
+import time;
+
+class Caller:
+  def __init__(self, name, topic):
+    self.name = name
+    self.topic = age
+    self.time = time.time()
+
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
+  if (msg.content === "maddox") {
+    msg.reply("Lost!")
   }
+  if (msg.content === "!call ") {
+    print time.time()
+    
+  }
+
 })
 client.login(process.env.BOT_TOKEN);
