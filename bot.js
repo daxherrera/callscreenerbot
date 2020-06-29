6e0d1e95d608a7e65bd7cdc35d57f650d59d8a69
daxherrera <daxherrera@gmail.com>
2020-06-29 01:59:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-  console.log(bot.guilds);
+  console.log('ne');
+  console.log(`Logged in as ${client.user.tag}!`);
 })
 
 client.on("message", msg => {
