49e91382adf9d2619608ba7fa772876214b33189
Dax Herrera <daxherrera@gmail.com>
2020-06-29 20:38:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on("message", msg => {
 		    console.log(call_list);
 		    for (i = 0; i < call_list.length; i++) { 
 		        console.log(call_list[i]);
-				msg.channel.send(call_list[i]);
+				msg.channel.send(call_list[i].user_name + " " + call_list[i].user_message);
 		    }
 		  })
 
