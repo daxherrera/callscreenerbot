f8ba41f81a5e0939885e1ea7c22a27a516be0696
Dax Herrera <daxherrera@gmail.com>
2020-06-29 21:05:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,8 @@ client.on("message", msg => {
 		      return console.error('Error executing query', err.stack)
 		    }
 		    var nowtime = Date.now();
-		    var new_call = {user_name: msg.author.username, user_id: msg.author.id, user_message : reason, timestamp : nowtime};
+		    DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		    var new_call = {user_name: msg.author.username, user_id: msg.author.id, user_message : reason, timestamp : dateFormat.format(nowtime)};
 		    //console.log(new_call);
 		    console.log("result.rows");
 		    console.log(result.rows.length);
